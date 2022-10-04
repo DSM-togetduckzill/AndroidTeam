0efcfd65e15fff95d117075bba3c2fc887f29f21
kimjjunho <kimjjun1007@gmail.com>
2022-10-04 19:06:02 +0900

    feat: chat fragment 추가

---
+++ app/src/main/java/com/example/tugedeongjilproject/chat/ChatFragment.kt
@@ -0,0 +1,14 @@
+package com.example.tugedeongjilproject.chat
+
+import com.example.tugedeongjilproject.R
+import com.example.tugedeongjilproject.base.BaseFragment
+import com.example.tugedeongjilproject.databinding.FragmentChatBinding
+
+class ChatFragment : BaseFragment<FragmentChatBinding>(R.layout.fragment_chat) {
+
+    override fun initView() {
+    }
+
+    override fun observeEvent() {}
+
+}
