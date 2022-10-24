1a4941c58bb9c6ddb7f8a1b0f855cc7e3309a8d6
kimjjunho <kimjjun1007@gmail.com>
2022-10-24 19:13:33 +0900

    WebViewHelperActivity추가

---
+++ app/src/main/java/com/example/tugedeongjilproject/WebViewHelperActivity.kt
@@ -0,0 +1,15 @@
+package com.example.tugedeongjilproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tugedeongjilproject.base.BaseActivity
+import com.example.tugedeongjilproject.databinding.ActivityWebViewHelperBinding
+
+class WebViewHelperActivity : BaseActivity<ActivityWebViewHelperBinding>(R.layout.activity_web_view_helper){
+    override fun initView() {
+
+    }
+
+    override fun observeEvent() {}
+
+}
