b2168a3923c53bd56e964cb949c3d72d6c32e60b
kimjjunho <kimjjun1007@gmail.com>
2022-10-31 23:41:34 +0900

    feat: friend list recycler view 추가

--- app/src/main/java/com/example/tugedeongjilproject/friend/FriendListFragment.kt
+++ app/src/main/java/com/example/tugedeongjilproject/friend/FriendListFragment.kt
@@ -5,15 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tugedeongjilproject.R
 import com.example.tugedeongjilproject.base.BaseFragment
 import com.example.tugedeongjilproject.databinding.FragmentFriendListBinding
 
 class FriendListFragment : BaseFragment<FragmentFriendListBinding>(R.layout.fragment_friend_list){
 
-    override fun initView() {
+    private val friendList = arrayListOf<FriendListData>().apply {
+        add(FriendListData("","제목","본문"))
+    }
 
+    override fun initView() {
+        binding.run {
+            rvFriend.run {
+                layoutManager = LinearLayoutManager(activity,LinearLayoutManager.VERTICAL,false)
+                rvFriend.setHasFixedSize(true)
+            }
+        }
     }
 
     override fun observeEvent() {}
+
+    override fun onResume() {
+        super.onResume()
+        binding.run {
+            rvFriend.adapter = FriendListAdapter(friendList)
+        }
+    }
 }
