73b7025ab2d401ee9e9dcbd66cca549c76be368b
kimjjunho <kimjjun1007@gmail.com>
2022-10-31 23:14:20 +0900

    feat: friend list fragment 추가

---
+++ app/src/main/java/com/example/tugedeongjilproject/friend/FriendListFragment.kt
@@ -0,0 +1,19 @@
+package com.example.tugedeongjilproject.friend
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tugedeongjilproject.R
+import com.example.tugedeongjilproject.base.BaseFragment
+import com.example.tugedeongjilproject.databinding.FragmentFriendListBinding
+
+class FriendListFragment : BaseFragment<FragmentFriendListBinding>(R.layout.fragment_friend_list){
+
+    override fun initView() {
+
+    }
+
+    override fun observeEvent() {}
+}
