915bc102f7ec77aab620ca5b66da96e99cf30c22
kimjjunho <kimjjun1007@gmail.com>
2022-11-08 11:02:33 +0900

    ?

---
+++ di/build/tmp/kapt3/stubs/debug/com/example/di/remote/RetrofitModule.java
@@ -0,0 +1,46 @@
+package com.example.di.remote;
+
+import java.lang.System;
+
+@dagger.hilt.InstallIn(value = {dagger.hilt.components.SingletonComponent.class})
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000&\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0004\b\u00c7\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u0010\u0010\u0003\u001a\u00020\u00042\u0006\u0010\u0005\u001a\u00020\u0006H\u0007J\b\u0010\u0007\u001a\u00020\bH\u0007J\u0010\u0010\t\u001a\u00020\n2\u0006\u0010\u000b\u001a\u00020\bH\u0007J\u0010\u0010\f\u001a\u00020\u00062\u0006\u0010\r\u001a\u00020\nH\u0007\u00a8\u0006\u000e"}, d2 = {"Lcom/example/di/remote/RetrofitModule;", "", "()V", "provideFriendListApi", "Lcom/example/data/remote/api/FriendListAPI;", "retrofit", "Lretrofit2/Retrofit;", "provideHttpLoggingInterceptor", "Lokhttp3/logging/HttpLoggingInterceptor;", "provideOkHttpClient", "Lokhttp3/OkHttpClient;", "httpLoggingInterceptor", "provideRetrofit", "okHttpClient", "di_debug"})
+@dagger.Module()
+public final class RetrofitModule {
+    @org.jetbrains.annotations.NotNull()
+    public static final com.example.di.remote.RetrofitModule INSTANCE = null;
+    
+    private RetrofitModule() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @dagger.Provides()
+    @javax.inject.Singleton()
+    public final okhttp3.logging.HttpLoggingInterceptor provideHttpLoggingInterceptor() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @dagger.Provides()
+    @javax.inject.Singleton()
+    public final okhttp3.OkHttpClient provideOkHttpClient(@org.jetbrains.annotations.NotNull()
+    okhttp3.logging.HttpLoggingInterceptor httpLoggingInterceptor) {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @dagger.Provides()
+    @javax.inject.Singleton()
+    public final retrofit2.Retrofit provideRetrofit(@org.jetbrains.annotations.NotNull()
+    okhttp3.OkHttpClient okHttpClient) {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @dagger.Provides()
+    @javax.inject.Singleton()
+    public final com.example.data.remote.api.FriendListAPI provideFriendListApi(@org.jetbrains.annotations.NotNull()
+    retrofit2.Retrofit retrofit) {
+        return null;
+    }
+}
