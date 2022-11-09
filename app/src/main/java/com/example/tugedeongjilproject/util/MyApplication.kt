d9b999730f6283b1b18dcd5371f103471d9132ba
kimjjunho <kimjjun1007@gmail.com>
2022-11-09 13:54:35 +0900

    fix

---
+++ app/src/main/java/com/example/tugedeongjilproject/util/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.seonsijo.util
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application()
+
