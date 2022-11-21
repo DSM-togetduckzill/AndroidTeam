e94497f09f13f32c48983baec6da988ff0775bb9
YangJIWon <gw893525@naver.com>
2022-11-21 18:44:53 +0900

    feat: splash class 내용

--- app/src/main/java/com/example/tugedeongjilproject/splash/Splash.kt
+++ app/src/main/java/com/example/tugedeongjilproject/splash/Splash.kt
@@ -1,4 +1,22 @@
 package com.example.tugedeongjilproject.splash
 
-class Splash {
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tugedeongjilproject.R
+import com.example.tugedeongjilproject.login.SignInActivity
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
 }
