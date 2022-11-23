8636c07bf08aeea6b1a87c1d9d2fc797c01a98c4
YangJIWon <gw893525@naver.com>
2022-11-23 22:42:16 +0900

    di

--- di/build/tmp/kapt3/stubs/debug/com/example/di/remote/RetrofitModule.java
+++ di/build/tmp/kapt3/stubs/debug/com/example/di/remote/RetrofitModule.java
@@ -3,7 +3,7 @@ package com.example.di.remote;
 import java.lang.System;
 
 @dagger.hilt.InstallIn(value = {dagger.hilt.components.SingletonComponent.class})
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u00004\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\b\u00c7\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u0010\u0010\u0003\u001a\u00020\u00042\u0006\u0010\u0005\u001a\u00020\u0006H\u0007J\b\u0010\u0007\u001a\u00020\bH\u0007J\u0018\u0010\t\u001a\u00020\n2\u0006\u0010\u000b\u001a\u00020\b2\u0006\u0010\f\u001a\u00020\rH\u0007J\u0010\u0010\u000e\u001a\u00020\u00062\u0006\u0010\u000f\u001a\u00020\nH\u0007J\u0010\u0010\u0010\u001a\u00020\u00112\u0006\u0010\u0005\u001a\u00020\u0006H\u0007\u00a8\u0006\u0012"}, d2 = {"Lcom/example/di/remote/RetrofitModule;", "", "()V", "provideFriendListApi", "Lcom/example/data/remote/api/FriendListAPI;", "retrofit", "Lretrofit2/Retrofit;", "provideHttpLoggingInterceptor", "Lokhttp3/logging/HttpLoggingInterceptor;", "provideOkHttpClient", "Lokhttp3/OkHttpClient;", "httpLoggingInterceptor", "authorizationInterceptor", "Lcom/example/data/interceptor/AuthorizationInterceptor;", "provideRetrofit", "okHttpClient", "provideSignInApi", "Lcom/example/data/remote/api/SignInAPI;", "di_debug"})
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000:\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\b\u00c7\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u0010\u0010\u0003\u001a\u00020\u00042\u0006\u0010\u0005\u001a\u00020\u0006H\u0007J\b\u0010\u0007\u001a\u00020\bH\u0007J\u0010\u0010\t\u001a\u00020\n2\u0006\u0010\u0005\u001a\u00020\u0006H\u0007J\u0018\u0010\u000b\u001a\u00020\f2\u0006\u0010\r\u001a\u00020\b2\u0006\u0010\u000e\u001a\u00020\u000fH\u0007J\u0010\u0010\u0010\u001a\u00020\u00062\u0006\u0010\u0011\u001a\u00020\fH\u0007J\u0010\u0010\u0012\u001a\u00020\u00132\u0006\u0010\u0005\u001a\u00020\u0006H\u0007\u00a8\u0006\u0014"}, d2 = {"Lcom/example/di/remote/RetrofitModule;", "", "()V", "provideFriendListApi", "Lcom/example/data/remote/api/FriendListAPI;", "retrofit", "Lretrofit2/Retrofit;", "provideHttpLoggingInterceptor", "Lokhttp3/logging/HttpLoggingInterceptor;", "provideMyPageAPI", "Lcom/example/data/remote/api/MyPageAPI;", "provideOkHttpClient", "Lokhttp3/OkHttpClient;", "httpLoggingInterceptor", "authorizationInterceptor", "Lcom/example/data/interceptor/AuthorizationInterceptor;", "provideRetrofit", "okHttpClient", "provideSignInApi", "Lcom/example/data/remote/api/SignInAPI;", "di_debug"})
 @dagger.Module()
 public final class RetrofitModule {
     @org.jetbrains.annotations.NotNull()
@@ -49,6 +49,14 @@ public final class RetrofitModule {
     @dagger.Provides()
     @javax.inject.Singleton()
     public final com.example.data.remote.api.SignInAPI provideSignInApi(@org.jetbrains.annotations.NotNull()
+    retrofit2.Retrofit retrofit) {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @dagger.Provides()
+    @javax.inject.Singleton()
+    public final com.example.data.remote.api.MyPageAPI provideMyPageAPI(@org.jetbrains.annotations.NotNull()
     retrofit2.Retrofit retrofit) {
         return null;
     }
